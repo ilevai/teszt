9fb7d51aeecd410200b21f86fbcc09565de5f9a5
ilevai <levai.istvan@uni-milton.hu>
2022-06-15 15:44:23 +0200

    #1 kezdetek

---
+++ alapok/01_HelloVilag/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01_HelloVilag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia!");
+
+            // komment, a fordító figyelmen kívül hagyja
+            // tetszőleges space és enter lehet a kódban
+            /*
+             * 
+             * többsoros
+             * megjegyzéshez
+             * haszáljuk ezt a fajta kommentelést
+             * 
+             */
+
+            // helyérzékeny kereső -> github -> roslyn (kódtár, Microsoft fejleszti)
+            // ; sorok végén kell, kivéve, ha blokkot zárunk: } ez után nem kell
+            // case sensitive: pl. console nem = a Console-lal
+        }
+    }
+}
